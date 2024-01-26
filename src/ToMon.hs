935a68189f7638ebea15e6efed164d94aadcdfc0
Job Hernandez <hj93@protonmail.com>
2024-01-25 20:10:27 -0800

    add support for a new expression

--- src/ToMon.hs
+++ src/ToMon.hs
@@ -16,6 +16,11 @@ data MonExp
 
 toMon :: Exp -> Int -> MonExp
 toMon (Int n) counter = AtmInt n
+toMon (Negative n) counter =
+  let tmpname = "temp_" ++ show counter in
+    let assi = MonLet tmpname (MonNegative n) in
+      SeqMon assi (AtmVar tmpname)
+    
 toMon (Var v) counter = AtmVar v
 toMon (Bool b) counter = AtmBool b
 toMon (Plus (Int e) (Int e2)) counter = MonPlus (toMon (Int e) counter) (toMon (Int e2) counter)
@@ -30,4 +35,21 @@ toMon (Exps e e2) counter =
   (SeqMon (toMon e counter) (toMon e2 counter2))
   where
     counter2 = counter + 1
+
+toMon (Let var (Plus (Int e) (Negative e2))) counter =
+  let monexp = toMon (Plus (Int e) (Negative e2)) counter
+      in
+    let letexp = getMonLetNegative monexp
+        plusexp = getMonPlus monexp
+        in
+      let assignment = MonLet var plusexp
+          in
+        SeqMon letexp assignment
+ 
+
+getMonLetNegative :: MonExp -> MonExp
+getMonLetNegative (SeqMon (MonLet s negative) (MonPlus e e2)) = (MonLet s negative)
+
+getMonPlus :: MonExp -> MonExp
+getMonPlus (SeqMon (MonLet s negative) (MonPlus e e2)) = (MonPlus e e2)
   
