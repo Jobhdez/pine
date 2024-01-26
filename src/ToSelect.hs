f476b75e8755aad18505390b4850c8c2463fbb83
Job Hernandez <hj93@protonmail.com>
2024-01-26 11:43:34 -0800

    add new select instructions matches

--- src/ToSelect.hs
+++ src/ToSelect.hs
@@ -2,6 +2,7 @@ module ToSelect where
 
 import Parser
 import ToMon
+
 data Imm = ImmInt Int | ImmStr String deriving Show
 data SelectExp =
   Instructions [(String, Imm, String)]
@@ -16,22 +17,33 @@ selectInstructions monexp =
 
 toSelect :: MonExp -> [(String, Imm, String)]
 toSelect (AtmInt n) = [("immediate",ImmInt n, "dummy")]
+
 toSelect (MonPlus (AtmInt n) (AtmInt n2)) =
   [("movq", ImmInt n, "tempvar"), ("addq", ImmInt n2, "tempVar")]
-toSelect (MonLet var (AtmInt n)) =
+
+toSelect (MonPlus (AtmVar v) (AtmInt e))  =
+  [("addq", ImmInt e, v)]
+  
+toSelect (MonLet var (AtmInt n))  =
   [("movq", ImmInt n, var)]
 
 toSelect (MonLet var (MonPlus (AtmInt n) (AtmInt n2))) =
   [("movq", ImmInt n, var), ("addq", ImmInt n2, var)]
 
-toSelect (MonLet var (MonPlus (AtmInt n) (AtmVar v))) =
+toSelect (MonLet var (MonPlus (AtmInt n) (AtmVar v)))  =
   [("movq", ImmInt n, var), ("addq", ImmStr v, var)]
-toSelect (MonLet var (MonNegative n)) =
+  
+toSelect (MonLet var (MonNegative n))  =
   [("movq", ImmInt n, var), ("subq", ImmInt 0, var)]
 
-toSelect (SeqMon (MonLet var e) (MonLet var2 (MonPlus e2 e3))) =
+toSelect (SeqMon (MonLet var e) (MonLet var2 (MonPlus e2 e3)))  =
   let firsts = toSelect (MonLet var e) in
     let seconds = toSelect (MonLet var2 (MonPlus e2 e3)) in
+      firsts ++ seconds
+
+toSelect (SeqMon e e2)  =
+  let firsts = toSelect e in
+    let seconds = toSelect e2 in
       firsts ++ seconds
   
           
