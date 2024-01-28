3f527175801f508d1e475eb0b68555b43f0cc5a8
Job Hernandez <hj93@protonmail.com>
2024-01-28 10:59:34 -0800

    add expose allocation pass

---
+++ src/ToExposeAlloc.hs
@@ -0,0 +1,67 @@
+module ToExposeAlloc where
+
+import Parser
+import ToMon
+import ToSelect
+import ToStack
+
+data Assignment = Assi String Int deriving Show
+data IfExpose = IfExpose CndExpose Int CollectExpose deriving Show
+data Allocate = Alloc String Int String deriving Show
+data AllocAssign = AllocAssign String Int String deriving Show
+data Assignments = Assigns [Assignment] deriving Show
+data AllocAssignments = AllocAssigns [AllocAssign] deriving Show
+
+data CndExpose = GlobalValue String Int String deriving Show
+data CollectExpose = Collect Int deriving Show
+
+
+data Begin =
+  Begin Assignments  IfExpose Allocate AllocAssignments String deriving Show
+
+makeBegin :: Exp -> Begin
+makeBegin exp =
+  let assignments = makeAssignments exp in
+    let assigns = getAssignments assignments in
+      let len = length assigns in
+        let ifexpose = makeifexp assignments in
+          let allocassigns = makeallocation assignments in
+            Begin assignments ifexpose (Alloc "var" len "tuple[int]") allocassigns "var"
+
+getAssignments :: Assignments -> [Assignment]
+getAssignments (Assigns e) = e
+        
+makeAssignments :: Exp -> Assignments
+makeAssignments assi =
+  let assigs = makeFstAssignments assi 0 in
+    Assigns assigs
+    
+makeFstAssignments :: Exp -> Int -> [Assignment]
+makeFstAssignments (TupleExp (Int n)) counter =
+  let tmpname = "x_" ++ show counter in
+    (Assi tmpname n) : []
+
+makeFstAssignments (TupleExp (Exps (Int n) (Int n2))) counter =
+  makeFstAssignments (TupleExp (Int n)) counter ++ makeFstAssignments (TupleExp (Int n2)) (counter + 1)
+
+makeFstAssignments (TupleExp (Exps e e2)) counter =
+  makeFstAssignments (TupleExp e) counter ++ makeFstAssignments (TupleExp e2) (counter + 1)
+                  
+makeifexp :: Assignments -> IfExpose
+makeifexp (Assigns assigs) =
+  let bytes = (length assigs) * 8 in
+    let global = GlobalValue "free_ptr" bytes "fromspace_end" in
+      let collect = Collect bytes in
+        IfExpose global 0 collect
+
+makeallocation :: Assignments -> AllocAssignments
+makeallocation assignments =
+  AllocAssigns assig
+  where
+    assig = makeallocationshelper assignments 0
+    
+makeallocationshelper :: Assignments -> Int -> [AllocAssign]
+makeallocationshelper (Assigns []) counter = []
+makeallocationshelper (Assigns ((Assi var n): xs)) counter =
+  let varname = "var_" ++ show counter in
+    AllocAssign varname counter var : makeallocationshelper (Assigns xs) (counter+1)
