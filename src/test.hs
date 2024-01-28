b827639b20a8012d10b144a1d3a065d4a8ffb16d
Job Hernandez <hj93@protonmail.com>
2024-01-28 14:19:04 -0800

    delete file

--- src/test.hs
+++
@@ -1,9 +0,0 @@
-toSSelectBeginAssignments :: Assignments -> [(String, Imm, String)]
-toSSelectBeginAssignments (Assigns e) =
-  toAssignmentsHelper e
-  
-toAssignmentsHelper :: [Assignments] -> [(String, Imm, String)]
-toAssignmentsHelper [] = []
-toAssignmentsHelper ((Assi var e):xs) =
-  ["movq", ImmInt e, var] : toAssignmentsHelper xs
-  
