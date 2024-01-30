f494ed567dc8fc9998cc29eb5102850f38a59a9b
Job Hernandez Lara <hj93@protonmail.com>
2024-01-30 13:35:43 -0800

    update

--- src/WhileIfExample.hs
+++ src/WhileIfExample.hs
@@ -1,9 +1,8 @@
-module IfExpExample where
+module WhileIfExample where
 
 import Utils
 
 main :: IO ()
 main = do
   let asm = runWhileIfexample "let x = 0;; while x < 4;: if x < 5; then print(x);; let x = x + 1;; else print(3);;"
-    -- Replace "outputFile.txt" with the desired file path
   writeToFile "whileifeg.s" asm
