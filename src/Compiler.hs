a1028b6c582035997685f2c56a17316e3bf4053b
Job Henandez Lara <hj93@protonmail.com>
2024-01-30 18:10:27 -0800

    Update Compiler.hs

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -9,28 +9,6 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.IO
 
-{-- example:
-suppose you have a tuple: (1,1,3).
-
-the 64 bit tag for this tuple would be 000 000011 1. You dont actually need
-the whole 64 bits. In assembly I will be using instructions that manipulate
-64 bits.
-
-the first three 0 bits are 0s because when processing a tuple you assign
-a 0 bit to a integer and 1 to a tuple.
-
-000011 on the other hand is the length of the tuple. in this case 3.
-
-the last bit represents whether its reachable from the root set.
-
-example using this code:
-
-> makeTag [Left 1, Left 1, Left 3]
---> 7
-the tag 7 will be placed in front of the tuple. this is done because the
-garbage collector needs to distinguish tuples from other data :-)
---}
-
 compileExp :: String -> String
 compileExp exp =
   let tokens = lexer exp in
