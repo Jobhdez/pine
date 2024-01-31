29620f84efea22a6f77ab3ad3a0327f4c0d1e5d2
Job Hernandez <hj93@protonmail.com>
2024-01-30 16:58:15 -0800

    refactor repetitive functions

--- src/WhileExample.hs
+++ src/WhileExample.hs
@@ -1,9 +1,9 @@
 module WhileExample where
 
-import Utils
+import Compiler
 
 main :: IO ()
 main = do
-  let asm = runWhileIfexample  "let x = 0;; while x < 5;: print(x);; let x = x + 1;;"
+  let asm = compileExp  "let x = 0;; while x < 5;: print(x);; let x = x + 1;;"
   writeToFile "whileeg.s" asm
 
--- src/WhileIfExample.hs
+++ src/WhileIfExample.hs
@@ -1,8 +1,8 @@
 module WhileIfExample where
 
-import Utils
+import Compiler
 
 main :: IO ()
 main = do
-  let asm = runWhileIfexample "let x = 0;; while x < 4;: if x < 5; then print(x);; let x = x + 1;; else print(3);;"
+  let asm = compileExp "let x = 0;; while x < 4;: if x < 5; then print(x);; let x = x + 1;; else print(3);;"
   writeToFile "whileifeg.s" asm
