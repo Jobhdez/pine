3d0cae142bd5bc0acc0de44f62267654588bc011
Job Hernandez <hj93@protonmail.com>
2024-02-04 18:12:58 -0800

    add examples

---
+++ src/ifceg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    if (2<3) {
+        printf("%d",2);
+}
+    else {
+        printf("%d",4);
+}
+}
---
+++ src/tupc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int x[4] = {4,5,6,4};
+    printf("%d",x[2]);
+
+}
